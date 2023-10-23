5154af6cf194f5bb856faf0926a8c1496f0a849b
ncocana <ncocana@cifpfbmoll.eu>
2023-10-23 20:33:16 +0200

    feat(navigation): show logout, login and register buttons

--- entregables_moodle/T04P02_login_system/navigation.php
+++ entregables_moodle/T04P02_login_system/navigation.php
@@ -20,7 +20,41 @@
                 </li>
             </ul>
             <?php
-            // login and logout options will be here
+                // check if users / customer was logged in
+                // if user was logged in, show "Edit Profile", "Orders" and "Logout" options
+                if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']==true && $_SESSION['access_level']=='Customer'){
+                    ?>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li <?php echo $page_title=="Edit Profile" ? "class='active'" : ""; ?>>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                <span class="glyphicon glyphicon-user" aria-hidden="true"></span>
+                                <?php echo $_SESSION['firstname']; ?>
+                                <span class="caret"></span>
+                            </a>
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="<?php echo $home_url; ?>logout.php">Logout</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <?php
+                }
+                // if user was not logged in, show the "login" and "register" options
+                else{
+                    ?>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li <?php echo $page_title=="Login" ? "class='active'" : ""; ?>>
+                            <a href="<?php echo $home_url; ?>login">
+                                <span class="glyphicon glyphicon-log-in"></span> Log In
+                            </a>
+                        </li>
+                        <li <?php echo $page_title=="Register" ? "class='active'" : ""; ?>>
+                            <a href="<?php echo $home_url; ?>register">
+                                <span class="glyphicon glyphicon-check"></span> Register
+                            </a>
+                        </li>
+                    </ul>
+                    <?php
+                }
             ?>
         </div><!--/.nav-collapse -->
     </div>
