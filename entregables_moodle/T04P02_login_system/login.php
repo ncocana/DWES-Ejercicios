37279fd12b0a03069ac959962ada04db1f79aeed
ncocana <ncocana@cifpfbmoll.eu>
2023-10-23 20:52:57 +0200

    feat(login): validate login credentials

--- entregables_moodle/T04P02_login_system/login.php
+++ entregables_moodle/T04P02_login_system/login.php
@@ -22,7 +22,27 @@ if($_POST){
     $user->email=$_POST['email'];
     // check if email exists, also get user details using this emailExists() method
     $email_exists = $user->emailExists();
-    // login validation will be here
+    // validate login
+    if ($email_exists && password_verify($_POST['password'], $user->password) && $user->status==1){
+        // if it is, set the session value to true
+        $_SESSION['logged_in'] = true;
+        $_SESSION['user_id'] = $user->id;
+        $_SESSION['access_level'] = $user->access_level;
+        $_SESSION['firstname'] = htmlspecialchars($user->firstname, ENT_QUOTES, 'UTF-8') ;
+        $_SESSION['lastname'] = $user->lastname;
+        // if access level is 'Admin', redirect to admin section
+        if($user->access_level=='Admin'){
+            header("Location: {$home_url}admin/index.php?action=login_success");
+        }
+        // else, redirect only to 'Customer' section
+        else{
+            header("Location: {$home_url}index.php?action=login_success");
+        }
+    }
+    // if username does not exist or password is wrong
+    else{
+        $access_denied=true;
+    }
 }
 // include page header HTML
 include_once "layout_head.php";
