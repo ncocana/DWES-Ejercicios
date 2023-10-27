eda4c79cdaae6995580271feb03a660ad73e35d0
ncocana <ncocana@cifpfbmoll.eu>
2023-10-27 16:51:11 +0200

    feat(reset_password): add "reset password" form

--- entregables_moodle/T04P02_login_system/reset_password.php
+++ entregables_moodle/T04P02_login_system/reset_password.php
@@ -24,7 +24,19 @@ echo "<div class='col-sm-12'>";
         die('Access code not found.');
     }
     else{
-        // reset password form will be here
+        // post code will be here
+        echo "<form action='" . htmlspecialchars($_SERVER["PHP_SELF"]) . "?access_code={$access_code}' method='post'>
+        <table class='table table-hover table-responsive table-bordered'>
+            <tr>
+                <td>Password</td>
+                <td><input type='password' name='password' class='form-control' required></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><button type='submit' class='btn btn-primary'>Reset Password</button></td>
+            </tr>
+        </table>
+        </form>";
     }
 echo "</div>";
 // include page footer HTML
