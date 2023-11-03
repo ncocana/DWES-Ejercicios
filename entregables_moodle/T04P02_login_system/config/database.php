49549855f8f3d901ad1a50adc1244734c090990a
ncocana <ncocana@cifpfbmoll.eu>
2023-11-03 20:50:45 +0100

    feat(database): add singletton

--- entregables_moodle/T04P02_login_system/config/database.php
+++ entregables_moodle/T04P02_login_system/config/database.php
@@ -8,11 +8,13 @@ class Database{
     public $conn;
     // get the database connection
     public function getConnection(){
-        $this->conn = null;
-        try{
-            $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-        }catch(PDOException $exception){
-            echo "Connection error: " . $exception->getMessage();
+        if ($this->conn == null){
+            // $this->conn = null;
+            try{
+                $this->conn = new PDO("pgsql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            }catch(PDOException $exception){
+                echo "Connection error: " . $exception->getMessage();
+            }
         }
         return $this->conn;
     }
