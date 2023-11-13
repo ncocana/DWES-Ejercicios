fe408ee0eac69d697e92611bae87040cf0b32969
ncocana <ncocana@cifpfbmoll.eu>
2023-11-13 23:28:39 +0100

    fix(core): fix home_url

--- entregables_moodle/T04P02_login_system/config/core.php
+++ entregables_moodle/T04P02_login_system/config/core.php
@@ -8,14 +8,18 @@ date_default_timezone_set('Europe/Madrid');
 // Determine the current domain
 if ($_SERVER['HTTP_HOST'] === 'ncocana.randion.es') {
     $domain = "ncocana.randion.es";
+    // home page url
+    $home_url = "http://" . $domain . "/dwes/ejercicios-1ra-eva-ncocana/entregables_moodle/T04P02_login_system/";
 } else if ($_SERVER['HTTP_HOST'] === 'ncocana_login.randion.es') {
     $domain = "ncocana_login.randion.es";
+    // home page url
+    $home_url = "http://" . $domain . "/";
 } else {
     // Default URL for unknown domains and localhost
     $domain = "localhost";
+    // home page url
+    $home_url = "http://" . $domain . "/dwes/ejercicios-1ra-eva-ncocana/entregables_moodle/T04P02_login_system/";
 }
-// home page url
-$home_url = "http://" . $domain . "/dwes/ejercicios-1ra-eva-ncocana/entregables_moodle/T04P02_login_system/";
 // page given in URL parameter, default page is one
 $page = isset($_GET['page']) ? $_GET['page'] : 1;
 // set number of records per page
