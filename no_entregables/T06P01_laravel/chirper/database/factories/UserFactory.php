aff59db0951b91c6a2a579edf312fa58bdafabde
ncocana <ncocana@cifpfbmoll.eu>
2023-12-11 21:01:53 +0100

    feat(app, database, resources): add lastname

--- no_entregables/T06P01_laravel/chirper/database/factories/UserFactory.php
+++ no_entregables/T06P01_laravel/chirper/database/factories/UserFactory.php
@@ -22,6 +22,7 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'lastname' => fake()->lastName(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
