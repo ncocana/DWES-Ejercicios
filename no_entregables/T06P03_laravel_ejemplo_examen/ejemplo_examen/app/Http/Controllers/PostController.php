84716c35f3647e06f9cdad9f787f9dbf598c08d4
ncocana <ncocana@cifpfbmoll.eu>
2024-01-15 19:18:27 +0100

    feat(Controllers, Requests): 6. create controller for Post

---
+++ no_entregables/T06P03_laravel_ejemplo_examen/ejemplo_examen/app/Http/Controllers/PostController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostUpdateRequest;
+use App\Models\Post;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(): View
+    {
+        return view('form.index', [
+            'posts' => Post::with('user')->latest()->get(),
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PostUpdateRequest $request): RedirectResponse
+    {
+        $validated = $request->user()->fill($request->validated());
+
+        $request->user()->posts()->create($validated);
+
+        return redirect(route('form.index'));
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post): View
+    {
+        $this->authorize('update', $post);
+
+        return view('form.edit', [
+            'post' => $post,
+        ]);
+    }
+}
