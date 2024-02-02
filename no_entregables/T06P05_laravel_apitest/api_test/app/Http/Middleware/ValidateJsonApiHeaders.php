8df343ced87b0f6a1c7d768d2b6a5ed97ffb196e
ncocana <ncocana@cifpfbmoll.eu>
2024-02-02 20:57:21 +0100

    feat(Middleware, tests, Kernel): practice tests to validate json api headers

---
+++ no_entregables/T06P05_laravel_apitest/api_test/app/Http/Middleware/ValidateJsonApiHeaders.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class ValidateJsonApiHeaders
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        // Devuelve el estado 406
+        // throw new HttpException(406);
+
+        if ($request->header('accept') != 'application/vnd.api+json') {
+            throw new HttpException(406, __('Not acceptable'));
+        }
+
+        if ($request->isMethod('POST') || $request->isMethod('PATCH')) {
+            if ($request->header('content-type') != 'application/vnd.api+json') {
+                throw new HttpException(415, __('Unsupported Media Type'));
+            }
+        }
+
+        return $next($request)->WithHeaders([
+            'content-type' => 'application/vnd.api+json'
+        ]);
+    }
+}
--- no_entregables/T06P05_laravel_apitest/api_test/tests/Feature/ValidateJsonApiHeadersTest.php
+++ no_entregables/T06P05_laravel_apitest/api_test/tests/Feature/ValidateJsonApiHeadersTest.php
@@ -2,19 +2,115 @@
 
 namespace Tests\Feature;
 
+use App\Http\Middleware\ValidateJsonApiHeaders;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Route;
 use Tests\TestCase;
 
 class ValidateJsonApiHeadersTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    // Create route for testing for this test
+    protected function setup():void {
+        parent::setup();
+        Route::any('test_route', fn() => 'Ok')->middleware(ValidateJsonApiHeaders::class);
+        Route::any('empty_response', fn() => response()->noContent())->middleware(ValidateJsonApiHeaders::class);
+    }
+
+    /** @test */
+    public function accept_header_must_be_present_in_all_request(): void
+    {
+        // Sin cabecera 'accept', devolverá un estado 406
+        $this->get('test_route')->assertStatus(406);
+
+        // Inspeccionar una respuesta con dump()
+        // Para que salga con mas claridad,
+        // en ".env" cambiar "APP_DEBUG" a  "APP_DEBUG=false"
+        // $this->get('test_route')->dump()->assertStatus(406);
+
+        // "APP_DEBUG=true"
+        // Con "APP_DEBUG=false", devuelve un JSON vacío con un mensaje vacío
+        // Para mostrar un mensaje no-vacío,
+        // "throw new HttpException(406, __('Not Acceptable'))" en el Middleware
+        // $this->getJson('test_route')->dump()->assertStatus(406);
+
+        // En la función 'get', el segundo parámetro puede contener las cabeceras
+        $this->get('test_route', [
+            'accept'=>'application/vnd.api+json'
+        ])->assertSuccessful();
+    }
+
+    /** @test */
+    public function content_type_header_must_be_present_in_all_post_request(): void
+    {
+        // Sin cabecera 'accept', devolverá un estado 406
+        $this->post('test_route')->assertStatus(406);
+
+        // Con la cabecera 'accept' pero sin la cabecera 'content-type',
+        // devolverá un estado 425
+        // El tercer parámetro contiene las cabeceras.
+        $this->post('test_route', [], [
+            'accept'=> 'application/vnd.api+json'
+        ])->assertStatus(415);
+
+        // Con ambas cabeceras, devolverá un estado entre 200 y 300
+        $this->post('test_route', [], [
+            'accept'=>'application/vnd.api+json',
+            'content-type'=>'application/vnd.api+json'
+        ])->assertSuccessful();
+    }
+
+    /** @test */
+    public function content_type_header_must_be_present_in_all_patch_request(): void
+    {
+        // Sin cabecera 'accept', devolverá un estado 406
+        $this->patch('test_route')->assertStatus(406);
+
+        // Con la cabecera 'accept' pero sin la cabecera 'content-type',
+        // devolverá un estado 425
+        // El tercer parámetro contiene las cabeceras.
+        $this->patch('test_route', [], [
+            'accept'=> 'application/vnd.api+json'
+        ])->assertStatus(415);
+
+        // Con ambas cabeceras, devolverá un estado entre 200 y 300
+        $this->patch('test_route', [], [
+            'accept'=>'application/vnd.api+json',
+            'content-type'=>'application/vnd.api+json'
+        ])->assertSuccessful();
+    }
+
+    /** @test */
+    public function content_type_header_must_be_present_in_all_request(): void
+    {
+        $this->get('test_route', [
+            'accept'=>'application/vnd.api+json'
+        ])->assertHeader('content-type', 'application/vnd.api+json');
+    }
+
+    /** @test */
+    public function content_type_header_must_not_be_present_in_empty_responses(): void
+    {
+        $this->delete('empty_response', [], [])->assertStatus(406);
+
+        $this->get('empty_response', [
+            'accept'=>'application/vnd.api+json'
+        ])->assertHeaderMissing('content-type');
+
+        $this->post('empty_response', [], [
+            'accept'=>'application/vnd.api+json',
+            'content-type' => 'application/vnd.api+json'
+        ])->assertHeaderMissing('content-type');
+
+        $this->patch('empty_response', [], [
+            'accept'=>'application/vnd.api+json',
+            'content-type' => 'application/vnd.api+json'
+        ])->assertHeaderMissing('content-type');
+
+        $this->delete('empty_response', [], [
+            'accept'=>'application/vnd.api+json'
+        ])->assertHeaderMissing('content-type');
     }
 }
