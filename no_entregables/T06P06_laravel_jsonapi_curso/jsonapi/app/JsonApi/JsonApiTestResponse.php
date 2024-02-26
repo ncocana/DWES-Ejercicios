e021db53d06a301caf250475be36ba909ac9afbb
ncocana <ncocana@cifpfbmoll.eu>
2024-02-26 17:28:14 +0100

    style(T06P06_laravel_jsonapi_curso): delete unused function

--- no_entregables/T06P06_laravel_jsonapi_curso/jsonapi/app/JsonApi/JsonApiTestResponse.php
+++ no_entregables/T06P06_laravel_jsonapi_curso/jsonapi/app/JsonApi/JsonApiTestResponse.php
@@ -45,38 +45,4 @@ class JsonApiTestResponse
             )->assertStatus(422);
         };
     }
-
-    protected function tryAlternativePointer(): Closure
-    {
-        return function($attribute) {
-            /** @var TestResponse $this */
-            $pointers = [
-                "/{$attribute}",
-                "/data/attributes/{$attribute}"
-            ];
-
-            $found = false;
-
-            foreach ($pointers as $pointer) {
-                try {
-                    $this->assertJsonFragment([
-                        'source' => ['pointer' => $pointer]
-                    ]);
-                    $found = true;
-                    // If found, break the loop
-                    break;
-                } catch (ExpectationFailedException $e) {
-                    // If not found, continue to the next pointer
-                    continue;
-                }
-            }
-
-            // If not found, return fail message
-            if (!$found) {
-                PHPUnit::fail("Failed to find a JSON:API validation error for key: '{$attribute}'"
-                    . PHP_EOL . PHP_EOL .
-                    $e->getMessage());
-            }
-        };
-    }
 }
